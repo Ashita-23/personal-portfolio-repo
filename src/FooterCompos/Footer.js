5e02fcefb53c16781d919c622c2330713ddb5298
Akash <akashbytes@gmail.com>
2023-05-06 00:43:38 +0530

    created footer files

---
+++ src/FooterCompos/Footer.js
@@ -0,0 +1,12 @@
+import "./footer.css"
+import heartImg from "..//Assets/Compo_images/heartFooter.png"
+
+const Footer = ()=>{
+    return(
+        <>
+            <div className="footer-text-box"><span>Made with</span><i className="fa-solid fa-heart heart-icone"></i><span>by Ashita</span></div>
+        </>
+    )
+}
+
+export default Footer 
