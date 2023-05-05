df83e8c8800a3990ed620220f18ec95fdb42093b
Akash <akashbytes@gmail.com>
2023-05-06 00:48:35 +0530

    created Skill_Assets component for skill details

---
+++ src/Assets/Skill_Assets/utils.js
@@ -0,0 +1,43 @@
+
+export const skillsName = [
+        {
+            id:"10A" ,
+            // imgs:html ,
+            name:"HTML",
+            url : "https://html.com/html5/"
+        } ,
+        {
+            id:"10B" ,
+            // imgs:css ,
+            name:"CSS" ,
+            url : "https://www.w3.org/TR/CSS/#css"
+        } ,
+        {
+            id:"10C" ,
+            // imgs:bootstrap ,
+            name:"BOOTSRAP" ,
+            url : "https://getbootstrap.com/docs/5.0/getting-started/introduction/"
+        } ,
+    
+        {
+            id:"10E" ,
+            // imgs:javascript ,
+            name:"JAVASCRIPT" ,
+            url : "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        } ,
+        {
+            id:"10F" ,
+            // imgs:react ,
+            name:"REACT" ,
+            url : "https://react.dev/"
+        } ,
+      
+        {
+            id:"10H" ,
+            // imgs:git ,
+            name:"GIT" ,
+            url : "https://git-scm.com/"
+        } ,
+    ]
+    
+    
