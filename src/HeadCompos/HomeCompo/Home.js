8ebabce2a2c053583813262e9f0967f4d0d3c0d1
Akash <akashbytes@gmail.com>
2023-05-05 16:52:21 +0530

    created basic home elements

--- src/HeadCompos/HomeCompo/Home.js
+++ src/HeadCompos/HomeCompo/Home.js
@@ -1,9 +1,21 @@
+import {useEffect , useState} from "react" 
 import "./home.css"
 
 const Home = () =>{
+
+
+
+
+
     return(<>
         <div className="home-outer">
-            <h1>My  name is Ashita</h1>
+        <div className="intro-outer">
+            <h1 className="name-text"><span className="first-text">Hello ,</span> I am Ashita </h1>
+            <p className="title-text-box"><span className="sec-text">{ "Frontend Developer" }</span></p>
+            <div className="home-btn-group">
+            <button className="home-btn"><i class="fa-brands fa-github"></i></button> {" "}<button className="home-btn"><i class="fa-brands fa-linkedin"></i></button>
+            </div>
+            </div>
         </div>
     </>)
 }
