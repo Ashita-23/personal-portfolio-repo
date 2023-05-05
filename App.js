725df67f40ef4bd378cc68b81d077437deecb165
Akash <akashbytes@gmail.com>
2023-05-06 00:51:09 +0530

    added Footer and SkillCounter in App.js

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client"
 import "/index.css"
 import Navigation from "./src/HeadCompos/NavCompo/Navigation"
 import Home from "./src/HeadCompos/HomeCompo/Home"
+import SkillCounter from "./src/BodyCompos/SkillCompos/SkillCounter"
+import Footer from "./src/FooterCompos/Footer"
 
 const App = () =>{
     return(<>
         <Navigation/>
         <Home/>
+        <SkillCounter/>
+        <Footer/>
     </>)
 }
 
