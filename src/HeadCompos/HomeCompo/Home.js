e78b5769b3fc44ae66e610035830976f3adf9bd3
Akash <akashbytes@gmail.com>
2023-05-05 15:05:58 +0530

    added src folder with Assets Head Body Footer components folder

---
+++ src/HeadCompos/HomeCompo/Home.js
@@ -0,0 +1,11 @@
+import "./home.css"
+
+const Home = () =>{
+    return(<>
+        <div className="home-outer">
+            <h1>My  name is Ashita</h1>
+        </div>
+    </>)
+}
+
+export default Home 
---
+++ src/HeadCompos/NavCompo/Navigation.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import "./navigation.css"
+import SunImg from "../../Assets/Compo_images/sun.png"
+import HalfMoon from "../../Assets/Compo_images/half-moon.png"
+import { useState } from "react"
+const Navigation = () => {
+
+    const [LightMode , setDarkMode] = useState(false)
+    
+    function DarkLightMode (){
+        if(LightMode === false){
+            setDarkMode(true)
+        }else if(LightMode ===true){
+            setDarkMode(false)
+        }
+    }
+
+
+    return(<>
+     <nav className="nav-outer">
+        {/* <div className="logo-box">Ashita</div> */}
+        <ul className="">
+            <li><i className="fa-solid fa-house-user"></i></li>
+            <li><i className="fa-regular fa-user"></i></li>
+            <li><i className="fa-solid fa-code"></i></li>
+            <li><i className="fa-regular fa-folder-open"></i></li>
+            <li><i className="fa-regular fa-paper-plane"></i></li>
+            </ul>
+        <div className="Dark-light-mode">
+           <button className="DLM-btn" onClick={()=>DarkLightMode()}>{LightMode?<img src={SunImg } alt="" className="DLM-img" />:<img src={HalfMoon} alt="" className="DLM-img" />}</button>
+        </div>
+       
+     </nav>
+    </>)
+}
+
+export default Navigation
