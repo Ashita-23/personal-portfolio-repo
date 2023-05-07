c8804b496a2c06799609d80fc40995fd95c9d413
Akash <akashbytes@gmail.com>
2023-05-07 21:49:55 +0530

    created ProjectCompos folder for Poject section

---
+++ src/BodyCompos/ProjectCompos/ProjectCounter.js
@@ -0,0 +1,16 @@
+
+import "./projectsCounter.css"
+
+const ProjectCounter = () =>{
+    return(<>
+        <div className="project-counter">
+            <h1 className="heading"> Projects</h1>
+            <div className="project-display">
+
+            </div>
+        </div>
+    </>)
+}
+
+
+export default ProjectCounter
