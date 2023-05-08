09ce97e69b86ecc0085352de0c93c26b6a3e4615
Akash <akashbytes@gmail.com>
2023-05-08 12:28:34 +0530

    created projectCounter and added some code fot counter / added projectCards.css for styling Cards .

---
+++ src/BodyCompos/ProjectCompos/ProjectCards.js
@@ -0,0 +1,23 @@
+
+import "./ProjectCards.css"
+
+const ProjectCards = ({ProjectHelper}) =>{
+    console.log(ProjectHelper)
+    return(<>
+  <div className="pro-card-outer">
+    {/* <figure > */}
+  <img src={ProjectHelper[0].imgs} className="pro-img" />
+    {/* </figure> */}
+    <div className="pro-deatil-box">
+        <span className="heading">{ProjectHelper[0].name}</span>
+        <span className="discreption">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit quasi, magni molestiae debitis odio natus!</span>
+        <div className="pro-btn-group">
+        <span className="link-btn"><a><i className="fa-solid fa-link"></i></a></span>
+        <span className="link-btn"><a><i className="fa-brands fa-github"></i></a></span>
+        </div>
+    </div>
+  </div>
+    </>)
+}
+
+export default ProjectCards
