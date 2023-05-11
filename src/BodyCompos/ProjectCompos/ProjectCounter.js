8cd703c6e1ab0fec6fc61bdcfcaa4469691712bf
Akash <akashbytes@gmail.com>
2023-05-11 21:22:44 +0530

    edited some in projectCoounter.js and projectCounter.css

--- src/BodyCompos/ProjectCompos/ProjectCounter.js
+++ src/BodyCompos/ProjectCompos/ProjectCounter.js
@@ -6,10 +6,12 @@ import ProjectCards from "./ProjectCards"
 const ProjectCounter = () =>{
     console.log(ProjectHelper)
     return(<>
-        <div className="project-counter">
-            <h1 className="heading"> Projects</h1>
+        <div className="project-counter" id="project">
+        <div className="project-inner-box">
+            <h1 className="pro-heading"> Projects</h1>
             <div className="project-display">
                 <ProjectCards ProjectHelper={ProjectHelper}/>
+            </div>
             </div>
         </div>
     </>)
