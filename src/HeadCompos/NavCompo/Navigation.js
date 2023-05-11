70fcf9215d386a86db9e18752e6d00759cd36d46
Akash <akashbytes@gmail.com>
2023-05-11 20:25:20 +0530

    added some code in nav

--- src/HeadCompos/NavCompo/Navigation.js
+++ src/HeadCompos/NavCompo/Navigation.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import "./navigation.css"
 import SunImg from "../../Assets/Compo_images/sun.png"
 import HalfMoon from "../../Assets/Compo_images/half-moon.png"
+import BG_Animation from "../../Assets/Animation/BG_Animaton"
 import { useState } from "react"
 const Navigation = () => {
 
@@ -19,12 +20,13 @@ const Navigation = () => {
     return(<>
      <nav className="nav-outer">
         {/* <div className="logo-box">Ashita</div> */}
+        <BG_Animation/>
         <ul className="">
-            <li><i className="fa-solid fa-house-user"></i></li>
-            <li><i className="fa-solid fa-circle-info"></i></li>
-            <li><i className="fa-solid fa-code"></i></li>
-            <li><i className="fa-regular fa-folder-open"></i></li>
-            <li><i className="fa-regular fa-paper-plane"></i></li>
+            <li><a href="#Home"><i className="fa-solid fa-house-user"></i></a></li>
+            <li><a href="#about"><i className="fa-solid fa-circle-info"></i></a></li>
+            <li><a href="#skills"><i className="fa-solid fa-code"></i></a></li>
+            <li><a href="#project"><i className="fa-regular fa-folder-open"></i></a></li>
+            <li><a href="#footer"><i className="fa-regular fa-paper-plane"></i></a></li>
             </ul>
         <div className="Dark-light-mode">
            <button className="DLM-btn" onClick={()=>DarkLightMode()}>{LightMode?<i className="fa-solid fa-lightbulb DM-img"></i>: <i className="fa-regular fa-lightbulb LM-img"></i>}</button>
