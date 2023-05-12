2ae30bacb7b5806447c27d293dbc2236826fcd24
Akash <akashbytes@gmail.com>
2023-05-12 13:49:52 +0530

    created contact form and added style in it

--- src/BodyCompos/ContactCompos/Contact.js
+++ src/BodyCompos/ContactCompos/Contact.js
@@ -1,24 +1,30 @@
 import "./contact.css"
+import ContactMail from "../../Assets/Compo_images/ContactMail.png"
 
 const Contact = ()=>{
     return(<>
         <div className="contact-outer" id="contact">
             <div className="contact-inner">
-            <h1 className="form-heding">Send Your Message </h1>
                 <form action="" className="form">
+            <h1 className="form-heding">Send Your Message </h1>
                     <lable className="lable">Name</lable>
-                        <input type="text" className="input-text"/>
+                        <input type="text" className="input-text" placeholder="jhon"/>
        
                     <lable className="lable">E-Mail</lable>
-                        <input type="email" className="input-text"/>
+                        <input type="email" className="input-text" placeholder="jhon@gmail.com"/>
               
                     <lable className="lable">Message</lable>
-                        <textarea type="text" className="input-textarea"/>
+                        <textarea type="text" className="input-textarea" placeholder="Type some thing ....."/>
                 
                     <lable className="lable">Send</lable>
-                        <input type="submit" className="input-submit"/>
+                        <input type="submit" className="input-submit" />
                     
                 </form>
+        
+                    <figure className="contact-image-outer">
+                        <img src={ContactMail}alt="" className="contact-image" />
+                    </figure>
+               
             </div>
         </div>
     </>)
